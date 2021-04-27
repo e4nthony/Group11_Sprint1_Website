8f0b6e29e9b50bcf5bdf3ac892a8253bf0b34a95
Anthony Ep <e4nthony@gmail.com>
2021-04-27 21:49:04 +0300

    rebase project
    - new index.js .
    - new site template .
    - added ejs dependency .

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app     = express()
+
+const port    = process.env.PORT || 4103
+
+const path = require('path')
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('public', path.join(__dirname, 'Public'))
+
+app.engine('.html', require('ejs').__express)
+app.use(express.static(path.join(__dirname, 'Public')))
+app.set('view engine', 'html')
+
+
+
+app.get('/', function (req, res) {
+    res.status(200).render('index')
+})
+
+app.get('/profile', (req, res) => {
+    res.send('this is Profile page')
+})
+
+app.get('/site_map', (req, res) => {
+    res.send('this is Site Map page')
+})
+
+app.get('/login', (req, res) => {
+    res.send('this is login page')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`server is up and running at: http://127.0.0.1:${port}`)
+    console.log(`http://127.0.0.1:${port}/`)
+})
