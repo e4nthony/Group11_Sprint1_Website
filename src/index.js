2d54909dc07b6651a8810c30834ab22bc0eb11ff
Anthony Ep <e4nthony@gmail.com>
2021-05-02 16:14:43 +0300

    added MongoDB functions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
+//----------- Site -----------
 const express = require('express')
 const app     = express()
-
 const port    = process.env.PORT || 4103
-
+//---
 const path = require('path')
-
 app.set('views', path.join(__dirname, 'views'))
 app.set('public', path.join(__dirname, 'Public'))
-
+//---
 app.engine('.html', require('ejs').__express)
 app.use(express.static(path.join(__dirname, 'Public')))
 app.set('view engine', 'html')
 
 
+//----------- MongoDB -----------
+const MongoClient = require('mongodb').MongoClient;
+//
+//
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    // const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+
+    client.close();
+});
+
+
+//----------- Redirections -----------
+
 // Landing Page | Home
 app.get('/', function (req, res) {
     res.status(200).render('index')
+})
+app.get('/login', (req, res) => {
+    res.send('this is login/register page ( sign in / sign up)')
 })
 
 app.get('/profile', (req, res) => {
@@ -26,7 +43,6 @@ app.get('/profile', (req, res) => {
 app.get('/about_doctor', (req, res) => {
     res.status(200).render('about_doctor_page')
 })
-
 
 app.get('/site_map', (req, res) => {
     res.send('this is Site Map page')
