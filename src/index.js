982b9d1c387c4c6b5c42a239089b0595eec8adc1
Anthony Ep <e4nthony@gmail.com>
2021-05-02 16:16:49 +0300

    updated MongoDB functions

--- src/index.js
+++ src/index.js
@@ -13,16 +13,16 @@ app.set('view engine', 'html')
 
 
 //----------- MongoDB -----------
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
 //
 //
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    // const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-
-    client.close();
-});
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//
+//     client.close();
+// });
 
 
 //----------- Redirections -----------
