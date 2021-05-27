1ebac0e076dba90b3f7147b7ddeeb8c09256703b
Alina Podoba <alinakn@icloud.com>
2021-05-27 11:50:22 +0300

    update

--- src/index.js
+++ src/index.js
@@ -39,73 +39,6 @@ async function main() {
 
     try {
         await client.connect()
-        app.get('/login', function (req, res) {
-            const { role } = req.user
-            if (role == -1 || role == undefined) {
-                return res.status(200).render('login')
-            }
-            alert('You are already logged in')
-            return res.redirect('/')
-        })
-        app.post('/', function (req, res) {
-            res.send('Got a POST request')
-        })
-
-
-        main().catch(console.error)
-
-//----------- Redirections -----------
-
-// Landing Page | Home
-        app.get('/', function (req, res) {
-            res.status(200).render('home')
-        })
-
-// Login/Registration Page | Sign in/up
-        app.get('/login', (req, res) => {
-            // res.send('this is login page')
-            res.status(200).render('login')
-        })
-
-// Login/Registration Page | Sign in/up-doctor
-        app.get('/doclogin', (req, res) => {
-            res.status(200).render('doclogin')
-        })
-
-// Doctor's public profile page | about_doctor_page
-        app.get('/about_doctor', (req, res) => {
-            res.status(200).render('about_doctor_page')
-        })
-
-// Template Page |
-        app.get('/inner-page', (req, res) => {
-            res.status(200).render('inner-page')
-        })
-
-// Search | Search results
-        app.get('/search', (req, res) => {
-            res.status(200).render('search')
-        })
-
-
-//todo: view different pages to doctor and to simple user by same link,
-//todo: for each user pages displays different depending on user id
-
-// // Simple/Doctor user page | private profile page
-// app.get('/profile', (req, res) => {
-//
-//     res.send('this is Profile page')
-// })
-
-// Simple_user_page | private profile page
-        app.get('/profile_s', (req, res) => {
-            res.status(200).render('simple_user_page')
-        })
-
-// Doctor_user_page | private profile page
-        app.get('/profile_d', (req, res) => {
-            res.status(200).render('doctor_user_page')
-        })
 
         await listDatabases(client)
 
@@ -118,6 +51,60 @@ async function main() {
 }
 
 
+main().catch(console.error)
+
+//----------- Redirections -----------
+
+// Landing Page | Home
+app.get('/', function (req, res) {
+    res.status(200).render('home')
+})
+
+// Login/Registration Page | Sign in/up
+app.get('/login', (req, res) => {
+    // res.send('this is login page')
+    res.status(200).render('login')
+})
+
+// Login/Registration Page | Sign in/up-doctor
+app.get('/doclogin', (req, res) => {
+    res.status(200).render('doclogin')
+})
+
+// Doctor's public profile page | about_doctor_page
+app.get('/about_doctor', (req, res) => {
+    res.status(200).render('about_doctor_page')
+})
+
+// Template Page |
+app.get('/inner-page', (req, res) => {
+    res.status(200).render('inner-page')
+})
+
+// Search | Search results
+app.get('/search', (req, res) => {
+    res.status(200).render('search')
+})
+
+
+//todo: view different pages to doctor and to simple user by same link,
+//todo: for each user pages displays different depending on user id
+
+// // Simple/Doctor user page | private profile page
+// app.get('/profile', (req, res) => {
+//
+//     res.send('this is Profile page')
+// })
+
+// Simple_user_page | private profile page
+app.get('/profile_s', (req, res) => {
+    res.status(200).render('simple_user_page')
+})
+
+// Doctor_user_page | private profile page
+app.get('/profile_d', (req, res) => {
+    res.status(200).render('doctor_user_page')
+})
 
 app.listen(port, () => {
     console.log(`server is up and running at: http://127.0.0.1:${port}`)
